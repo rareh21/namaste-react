7e32436327348e0156c71108807ae63d7440cb9d
Hemant Kumar <hemantkumarh828@gmail.com>
2024-08-15 17:12:22 +0530

    setup

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//React Element 
+const heading = (
+    
+)
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(parent);
